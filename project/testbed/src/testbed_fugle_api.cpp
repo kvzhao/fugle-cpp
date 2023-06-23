81d1b57a6415750127a3be344923ac946f680508
kvzhao <kelispinor@gmail.com>
2023-06-24 00:19:05 +0800

    update: some modification of testbed app

--- project/testbed/src/testbed_fugle_api.cpp
+++ project/testbed/src/testbed_fugle_api.cpp
@@ -12,19 +12,15 @@ using namespace fugle_realtime;
 using namespace tabulate;
 
 struct Args {
-    string endpoint;
     string symbol = "2330";
-    bool showApiList = false;
     bool debug = false;
 };
 
 int main(int argc, char **argv) {
-    CLI::App app{"Fugle Terminal"};
+    CLI::App app{"Fugle API Testbed: for terminal development"};
 
     Args args;
     app.add_option("-s,--stock-symbol", args.symbol, "symbol");
-    app.add_option("-e,--endpoint", args.endpoint, "endpoint");
-    app.add_flag("-l,--show-api-list", args.showApiList, "print Fugle APIs");
     app.add_flag("-d,--set-debug-mode", args.debug, "set debug log");
 
     try {
@@ -33,135 +29,122 @@ int main(int argc, char **argv) {
         return app.exit(e);
     }
 
-    if (args.showApiList) {
-        spdlog::info("SQlite3 version {} {} ", SQLite::VERSION,
-                     SQLite::getLibVersion());
-        spdlog::info("SQliteCpp version {} ", SQLITECPP_VERSION);
-        return ShowAPIs();
-    }
-
     if (args.debug) {
         spdlog::set_level(spdlog::level::debug);
         spdlog::debug("Set to debug log");
     }
 
-    FugleHttpClientBase fugleClient;
+    // NOTE: Consider combine
     FugleIntraday intraday;
     FugleSnapshot snapshot;
     FugleHistorical historical;
 
-    if (args.endpoint.empty()) {
+    auto actives = snapshot.Actives(
+        {.market = MarketType::TSE, .trade = TradeType::VALUE});
 
-        auto actives = snapshot.Actives(
-            {.market = MarketType::TSE, .trade = TradeType::VALUE});
+    // TODO: Implement this as a specific function
+    // Ranking of Transaction Values
+    tabulate::Table table;
+    table.add_row(
+        {"Rank", "Symbol", "Value", "Volume", "Change", "Change (%)", "Name"});
+    table.format().multi_byte_characters(true);
 
-        tabulate::Table table;
-        table.add_row({"Rank", "Symbol", "Value", "Volume", "Change", "Name"});
-        table.format().multi_byte_characters(true);
-
-        uint32_t topStocks = std::min(100, (int)actives.data.size());
-        for (uint32_t i = 0; i < topStocks; ++i) {
-            uint32_t rowIndex = i + 1;
-            const auto &data = actives.data[i];
-            auto valueStr = floatToString(data.tradeValue / 1e8);
-            table.add_row({
-                to_string(rowIndex),
-                data.symbol,
-                valueStr,
-                to_string(data.tradeVolume),
-                floatToString(data.change),
-                data.name,
-            });
-            if (data.change > 0) {
-                table[rowIndex][4].format().font_background_color(Color::red);
-            } else {
-                table[rowIndex][4].format().font_background_color(Color::green);
-            }
-        }
-
-        cout << table << endl;
-
-        auto movers = snapshot.Movers({
-            .market = MarketType::TSE,
-            .direction = MoveDirectionType::UP,
-            .gte = 5,
+    uint32_t topStocks = std::min(100, (int)actives.data.size());
+    for (uint32_t i = 0; i < topStocks; ++i) {
+        uint32_t rowIndex = i + 1;
+        const auto &data = actives.data[i];
+        auto valueStr = floatToString(data.tradeValue / 1e8);
+        float changePercent =
+            data.change / (data.closePrice - data.change) * 100.0;
+        table.add_row({
+            to_string(rowIndex),
+            data.symbol,
+            valueStr,
+            to_string(data.tradeVolume),
+            floatToString(data.change),
+            floatToString(changePercent),
+            data.name,
         });
-
-        for (const auto &data : movers.data) {
-            spdlog::info("{} = {} -> {}", data.name, data.openPrice,
-                         data.closePrice);
-        }
-
-        auto quotes = snapshot.Quotes({
-            .market = MarketType::TSE,
-        });
-
-        auto candles =
-            historical.Candles({.symbol = args.symbol,
-                                .from = Date{2023, 5, 19},
-                                .to = Date{2023, 5, 26},
-                                .timeframe = CandleTimeFrame::K_DAY});
-
-        for (const auto &kbar : candles.data) {
-            spdlog::info("[{}] open {}, close {}, high {}, low {}, volume {}",
-                         kbar.date, kbar.open, kbar.close, kbar.high,
-                         kbar.close, kbar.volume);
-        }
-
-        // auto vol = intraday.Volumes({.symbol = args.symbol});
-        // spdlog::info("vol.symbol {}", vol.symbol);
-        // spdlog::info("vol.date {}", vol.date);
-        // for (const auto &data : vol.data) {
-        //   spdlog::info("price = {} @ volume = {}, ask/bid = ({}, {})",
-        //   data.price,
-        //                data.volume, data.volumeAtAsk, data.volumeAtBid);
-        // }
-
-        // auto quote = intraday.Quote({.symbol = args.symbol});
-        // spdlog::info("quote.name {}", quote.name);
-        // spdlog::info("quote.lastPrice {}", quote.lastPrice);
-        // spdlog::info("quote.total.tradeValue {}", quote.total.tradeValue);
-        // spdlog::info("quote.total.tradeVolume {}", quote.total.tradeVolume);
-        // spdlog::info("quote.total.tradeVolumeAtAsk {}",
-        //              quote.total.tradeVolumeAtAsk);
-        // spdlog::info("quote.total.tradeVolumeAtBid {}",
-        //              quote.total.tradeVolumeAtBid);
-
-        // auto trades = intraday.Trades({.symbol = args.symbol});
-
-        // for (const auto &data : trades.data) {
-        //   spdlog::debug("trade p / v = {} / {} ", data.price, data.volume);
-        // }
-
-        // auto intradyCandles = intraday.Candles(
-        //     {.symbol = args.symbol, .timeFrame = CandleTimeFrame::K_10_MIN});
-
-        // for (const auto &data : intradyCandles.data) {
-        //   spdlog::debug("candle {}, {}, {}, {} ", data.open, data.high,
-        //   data.low,
-        //                 data.close);
-        // }
-
-        // auto tickers = intraday.Tickers({.type = TickerType::EQUITY,
-        //                                  .exchange = ExchangeType::TWSE,
-        //                                  .market = MarketType::TSE});
-
-        // for (const auto &ticker : tickers.data) {
-        //   spdlog::debug("{} {}", ticker.symbol, ticker.name);
-        // }
-        // spdlog::info("# of tickers {}", tickers.data.size());
-
-    } else {
-        string request;
-
-        if (!args.symbol.empty()) {
-            request = joinWithSlash({args.endpoint, args.symbol});
+        if (data.change > 0) {
+            table[rowIndex][4].format().font_color(Color::red);
+            table[rowIndex][5].format().font_color(Color::red);
         } else {
-            request = args.endpoint;
+            table[rowIndex][4].format().font_color(Color::green);
+            table[rowIndex][5].format().font_color(Color::green);
         }
-        auto response = fugleClient.SimpleGet(request);
-        spdlog::info("Response: {}", response);
     }
+
+    cout << table << endl;
+
+    auto movers = snapshot.Movers({
+        .market = MarketType::TSE,
+        .direction = MoveDirectionType::UP,
+        .gte = 5,
+    });
+
+    // for (const auto &data : movers.data) {
+    //     spdlog::info("{} = {} -> {}", data.name, data.openPrice,
+    //                  data.closePrice);
+    // }
+
+    auto quotes = snapshot.Quotes({
+        .market = MarketType::TSE,
+    });
+
+    // For Drawing Candles
+    auto candles = historical.Candles({.symbol = args.symbol,
+                                       .from = Date{2023, 5, 19},
+                                       .to = Date{2023, 5, 26},
+                                       .timeframe = CandleTimeFrame::K_DAY});
+
+    for (const auto &kbar : candles.data) {
+        spdlog::debug("[{}] open {}, close {}, high {}, low {}, volume {}",
+                      kbar.date, kbar.open, kbar.close, kbar.high, kbar.close,
+                      kbar.volume);
+    }
+
+    // auto vol = intraday.Volumes({.symbol = args.symbol});
+    // spdlog::info("vol.symbol {}", vol.symbol);
+    // spdlog::info("vol.date {}", vol.date);
+    // for (const auto &data : vol.data) {
+    //   spdlog::info("price = {} @ volume = {}, ask/bid = ({}, {})",
+    //   data.price,
+    //                data.volume, data.volumeAtAsk, data.volumeAtBid);
+    // }
+
+    // auto quote = intraday.Quote({.symbol = args.symbol});
+    // spdlog::info("quote.name {}", quote.name);
+    // spdlog::info("quote.lastPrice {}", quote.lastPrice);
+    // spdlog::info("quote.total.tradeValue {}", quote.total.tradeValue);
+    // spdlog::info("quote.total.tradeVolume {}", quote.total.tradeVolume);
+    // spdlog::info("quote.total.tradeVolumeAtAsk {}",
+    //              quote.total.tradeVolumeAtAsk);
+    // spdlog::info("quote.total.tradeVolumeAtBid {}",
+    //              quote.total.tradeVolumeAtBid);
+
+    // auto trades = intraday.Trades({.symbol = args.symbol});
+
+    // for (const auto &data : trades.data) {
+    //   spdlog::debug("trade p / v = {} / {} ", data.price, data.volume);
+    // }
+
+    // auto intradyCandles = intraday.Candles(
+    //     {.symbol = args.symbol, .timeFrame = CandleTimeFrame::K_10_MIN});
+
+    // for (const auto &data : intradyCandles.data) {
+    //   spdlog::debug("candle {}, {}, {}, {} ", data.open, data.high,
+    //   data.low,
+    //                 data.close);
+    // }
+
+    // auto tickers = intraday.Tickers({.type = TickerType::EQUITY,
+    //                                  .exchange = ExchangeType::TWSE,
+    //                                  .market = MarketType::TSE});
+
+    // for (const auto &ticker : tickers.data) {
+    //   spdlog::debug("{} {}", ticker.symbol, ticker.name);
+    // }
+    // spdlog::info("# of tickers {}", tickers.data.size());
 
     return 0;
 }
